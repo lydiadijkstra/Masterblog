f3d208167a7845057c45cb32793edc5ecb896f7b
Lydia Dijkstra <lydiadijkstra@Lydias-MacBook-Air.local>
2024-11-13 00:39:21 +0100

    adds the add-new-post function and html file

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template, redirect, url_for
 import blogpost_storage
 
 app = Flask(__name__)
@@ -9,6 +9,19 @@ def index():
     blog_posts = blogpost_storage.read_blogposts()
     print(blog_posts)
     return render_template('index.html', post=blog_posts)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        author = request.form.get("author")
+        title = request.form.get("title")
+        content = request.form.get("new_post")
+
+        blogpost_storage.add_blogpost(title=title, author=author, content=content)
+
+        return redirect(url_for("index"))
+    return render_template('add.html')
 
 
 if __name__ == "__main__":
