330d1bb385d9feba0ee393ceeee0a202211dc923
Lydia Dijkstra <lydiadijkstra@Lydias-MacBook-Air.local>
2024-11-14 00:02:40 +0100

    adds the delete function, creates the update-function. Updates the CSS to make all pages have similar look

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """
+    Render the index page displaying all listed blog posts
+    :return: index template
+    """
     blog_posts = blogpost_storage.read_blogposts()
     print(blog_posts)
     return render_template('index.html', post=blog_posts)
@@ -13,16 +17,58 @@ def index():
 
 @app.route('/add', methods=['GET', 'POST'])
 def add():
+    """
+    Handles new blogposts
+    :return: add.html
+    """
     if request.method == 'POST':
         author = request.form.get("author")
         title = request.form.get("title")
         content = request.form.get("new_post")
-
         blogpost_storage.add_blogpost(title=title, author=author, content=content)
-
         return redirect(url_for("index"))
     return render_template('add.html')
 
 
+@app.route('/delete/<int:post_id>', methods=['POST'])
+def delete(post_id):
+    """
+    Handles deleting blogposts by passing the ID of the post
+    :param post_id: id of blogpost to delete
+    :return: index.html
+    """
+    blogpost_storage.delete_blogpost(id=post_id)
+    return redirect(url_for("index"))
+
+
+@app.route('/delete/<int:post_id>')
+def delete_confirmation(post_id):
+    """
+    Handles the confirmation for deleting blogposts
+    :param post_id:
+    :return:
+    """
+    return render_template('delete.html', post_id=post_id)
+
+
+@app.route('/update/<int:post_id>', methods=['GET', 'POST'])
+def update(post_id):
+    """
+    Handles updating existing blogposts
+    :param post_id: the id of the blogpost
+    :return: update.html
+    """
+    post = blogpost_storage.fetch_post_by_id(post_id)
+    if post is None:
+        return "Post not found", 404
+    if request.method == 'POST':
+        title = request.form.get("title")
+        content = request.form.get("content")
+        blogpost_storage.update_blogpost(id=post_id, title=title, content=content)
+        return redirect(url_for("index"))
+    return render_template('update.html', post=post)
+
+
 if __name__ == "__main__":
+    """Runs the app on port 5001"""
     app.run(host="0.0.0.0", port=5001, debug=True)
--- blogpost_storage.py
+++ blogpost_storage.py
@@ -5,6 +5,10 @@ JSON_FILE = "blogposts_data.json"
 
 
 def read_blogposts():
+    """
+    Fetches the blogposts from the JSON file, if no post it creates an empty list
+    :return: blogposts
+    """
     try:
         with open(JSON_FILE, "r", encoding="utf-8") as file:
             blog_posts = json.load(file)
@@ -14,11 +18,21 @@ def read_blogposts():
 
 
 def save_blogposts(blog_posts):
+    """
+    Saves the new data to the JSON File
+    :param blog_posts: new list of posts
+    """
     with open(JSON_FILE, "w", encoding="utf-8") as file:
         json.dump(blog_posts, file, indent=4)
 
 
 def add_blogpost(author, title, content):
+    """
+    Handles adding new blogposts to the JSON file
+    :param author: text, author of post
+    :param title: text, title of post
+    :param content: text, content of post
+    """
     blog_posts = read_blogposts()
     autoincrement_id = blog_posts[-1]["id"] +1 if blog_posts else 1
 
@@ -34,24 +48,44 @@ def add_blogpost(author, title, content):
 
 
 def update_blogpost(id, title, content):
+    """
+    Handles updating the blogposts in the JSON file
+    :param id: integer, post id
+    :param title: text, author of post
+    :param content: text, post content
+    """
     blog_posts = read_blogposts()
     for post in blog_posts:
         if post["id"] == id:
             if title:
-                post["title"] == title
+                post["title"] = title
             if content:
-                post["content"] == content
+                post["content"] = content
             break
     else:
         print("No blogpost was found with that ID {id}")
         return
-
     save_blogposts(blog_posts)
 
 
 def delete_blogpost(id):
+    """
+    Handles deleting posts from the JSON file
+    :param id: integer, post id
+    """
     blog_posts = read_blogposts()
-
     blog_posts = [post for post in blog_posts if post["id"] != id]
-
     save_blogposts(blog_posts)
+
+
+def fetch_post_by_id(post_id):
+    """
+    Find a post from the JSON file by handing in the unique post id
+    :param post_id: integer, post id
+    :return: the post belonging to the id
+    """
+    blog_posts = read_blogposts()
+    for post in blog_posts:
+        if post["id"] == post_id:
+            return post
+    return None
